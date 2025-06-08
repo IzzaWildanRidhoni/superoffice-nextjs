2410e089836a256a4fc95594f4fbb5b7c6ea259f
izza wildan ridhoni <izzawildan88@gmail.com>
2025-06-08 15:55:44 +0700

    Tampilkan data details office

--- features/offices/data/officeSpaces.mock.ts
+++ features/offices/data/officeSpaces.mock.ts
@@ -3,15 +3,15 @@ import { OfficeSpace } from "../types/officeSpace.types";
 export const officeSpaces: OfficeSpace[] = [
   {
     id: 1,
-    title: "Wildan Park Central Master Silicon Valley Star Class",
+    title: "Wildan Park Central Master",
     slug: "wildan-park-central-master-silicon-valley-star-class",
     price: 1000000,
     duration: "20 Days",
     address: "Jl. Pahlawan No. 1, Jakarta",
-    about: "About Office Space 1",
+    about: "About Office Space 1 lorem ipsum dolor sit amet consectetur adipiscing elit  lorem ipsum dolor sit amet consectetur adipiscing elits ",
     location: "Jakarta",
     rating: 4.5,
-    tags: ["Popular", "Star Class", "Secure"],
+    tags: ["Popular"],
     image: "/assets/images/thumbnails/thumbnails-1.png",
     images: [
       "/assets/images/thumbnails/thumbnail-details-1.png",
@@ -46,10 +46,10 @@ export const officeSpaces: OfficeSpace[] = [
     price: 18560000,
     duration: "20 Days",
     address: "Jl. Pahlawan No. 1, Surabaya",
-    about: "About Remote Office Space",
+    about: "About Remote Office Space lorem ipsum dolor sit amet consectetur adipiscing elit  lorem ipsum dolor sit amet consectetur adipiscing elits",
     location: "Surabaya",
     rating: 4.9,
-    tags: ["Favorite", "Star Class", "Secure"],
+    tags: ["Favorite"],
     image: "/assets/images/thumbnails/thumbnails-3.png",
     images: [
       "/assets/images/thumbnails/thumbnail-details-2.png",
